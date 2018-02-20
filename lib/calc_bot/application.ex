68db458454fa791c8c3a625c1bef5ddadb0838f2
smaximov <s.maximov@teachmeplease.com>
2018-02-20 18:20:15 +0300

    Remove orphaned import

--- lib/calc_bot/application.ex
+++ lib/calc_bot/application.ex
@@ -8,8 +8,6 @@ defmodule CalcBot.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    import Supervisor.Spec
-
     # Define workers and child supervisors to be supervised
     children = [
       CalcBot.Repo,
