13f88936ab4705e196c75384478e067f604b4c91
smaximov <s.maximov@teachmeplease.com>
2018-02-20 13:12:06 +0300

    Fix scanning floats

--- test/calc_bot/calculator/lexer_test.exs
+++ test/calc_bot/calculator/lexer_test.exs
@@ -1,5 +1,13 @@
 defmodule CalcBot.Calculator.LexerTest do
   use ExUnit.Case, async: true
 
+  alias CalcBot.Calculator.Lexer
+
   doctest CalcBot.Calculator.Lexer
+
+  test "scans floats" do
+    assert Lexer.scan("1.2") == {:ok, [{:float, 1, 1.2}]}
+    assert Lexer.scan("1.2e12") == {:ok, [{:float, 1, 1.2e12}]}
+    assert Lexer.scan("1.2E-12") == {:ok, [{:float, 1, 1.2e-12}]}
+  end
 end
