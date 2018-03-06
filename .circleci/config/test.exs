c7f0eadc077842bb531f7d6df6569074cc3c4c58
smaximov <s.maximov@teachmeplease.com>
2018-03-06 19:16:54 +0300

    Setup CircleCI

---
+++ .circleci/config/test.exs
@@ -0,0 +1,22 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :calc_bot, CalcBotWeb.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :calc_bot, CalcBot.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  database: "circleci-test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+config :argon2_elixir,
+  t_cost: 1,
+  m_cost: 8
